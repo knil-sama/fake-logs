37d982cca49e7c805943014e66023b606e46cc91
Clement Demonchy <clement.demonchy@jobteaser.com>
2018-06-30 01:14:12 +0200

    Revert ""Added minimal setup for install""

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
+
+# new versions of pip requires a session
+requirements = pip.req.parse_requirements(
+    'requirements.txt', session=pip.download.PipSession()
+)
+
+for item in requirements:
+    if getattr(item, 'link', None):  # newer pip has link
+        links.append(str(item.link))
+    if item.req:
+        requires.append(str(item.req)) # always the package name 
+
 
 setup(name='fake-logs',
       version='1.0',
       description='Python server fake log generator',
       author='s4tori',
+      packages=find_packages(),
       url='https://github.com/s4tori/fake-logs',
+      packages=['fake_logs'],
+      install_requires=requires,
+      dependency_links=links,
       scripts=['fake-logs.py']
 )

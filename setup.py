bcec72b7af8e9b45dc6fd1a627beaf90c7d47f0f
Clement Demonchy <clement.demonchy@jobteaser.com>
2018-06-30 01:12:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(name='fake-logs',
       description='Python server fake log generator',
       author='s4tori',
       url='https://github.com/s4tori/fake-logs',
-      packages=['fake_logs'],
       scripts=['fake-logs.py']
 )
